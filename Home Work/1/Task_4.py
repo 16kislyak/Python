76a0b3e7d9410c318429294454e95083796fae67
16kislyak <16kislyak@gmail.com>
2023-01-06 17:03:24 +0300

    added task 1 (seminar 12)

--- Home Work/1/Task_4.py
+++ Home Work/1/Task_4.py
@@ -13,3 +13,17 @@ elif num == 4:
     print('X > 0, Y < 0')
 else: 
     print('Неверный номер')
+
+# quarter = input()
+
+# match quarter:
+#    case "1":
+#    print("x > 0, y > 0")
+#    case "2":
+#    print("x < 0, y > 0")
+#    case "3":
+#    print("x < 0, y < 0")
+#    case "4":
+#    print("x > 0, y < 0")
+#    case _:
+#    print("error")
