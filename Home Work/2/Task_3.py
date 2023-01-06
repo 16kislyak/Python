88b917545dc788625cac02e3a727f34880680686
16kislyak <16kislyak@gmail.com>
2023-01-06 19:25:33 +0300

    added task 3 (homework)

--- Home Work/2/Task_3.py
+++ Home Work/2/Task_3.py
@@ -6,5 +6,5 @@
 
 n = int(input("Введите число: "))
 list = [round((1+1/i)**i, 3) for i in range(1, n+1)]
-print(f'Последовательность: {list}\nСумма: {round(sum(list), 3)}')
+print(f'{list}\nСумма: {round(sum(list), 3)}')
 
